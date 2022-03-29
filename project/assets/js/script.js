7cdb1baa6730078689da3e84a1fda2156cec717f
Abyou <emandaabyou@gmail.com>
2022-03-29 03:56:47 -0500

    add ability to capture form field values

--- project/assets/js/script.js
+++ project/assets/js/script.js
@@ -4,9 +4,33 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 var createTaskHandler = function (event) {
   event.preventDefault();
 
-  listItemEl = document.createElement("li");
-  listItemEl.className = "task-item";
-  listItemEl.textContent = "Please add your task";
+  var taskNameInput = document.querySelector("input[name = 'task-name']").value;
+  var taskTypeInput = document.querySelector(
+    "select[name = 'task-type']"
+  ).value;
+
+  // create list item
+  var listItemEl = document.createElement("li");
+  listItemEl.className = "taskItem";
+
+  //create div to hold task info and add to list item
+  var taskInfoEl = document.createElement("div");
+
+  // give it a class name
+  taskInfoEl.classname = "task-info";
+
+  // add HTML content ot div
+  taskInfoEl.innerHTML =
+    "<h3 class='task-name'>" +
+    taskNameInput +
+    "</h3><span class='task-type'>" +
+    taskTypeInput +
+    "</span>";
+
+  listItemEl.appendChild(taskInfoEl);
+
+  // add entire list item to list
+
   tasksToDoEl.appendChild(listItemEl);
 };
 
